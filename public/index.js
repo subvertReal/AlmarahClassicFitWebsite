3d0fe4f01dff04989e7392948e85745877c8e617
subvert <sherjeelk2004@gmail.com>
2024-06-25 21:28:20 -0400

    0.05 - Minor

---
+++ public/index.js
@@ -0,0 +1,14 @@
+var btn = document.getElementById('button1');
+
+btn.addEventListener('click', function() {
+
+    window.open('https://wa.me/+19054974684', '_blank');
+});
+
+
+var btn = document.getElementById('button2');
+
+btn.addEventListener('click', function() {
+
+    window.open('https://wa.me/+19054974684', '_blank');
+});
